aca86c6fc4ecdb487af4268d0c425ea31354ccc4
Gabriel Packer <ggkpacker@gmail.com>
2018-05-04 18:02:14 -0300

    create initialize for fibonacci_sequence

--- app/controllers/fibonacci_controller/fibonacci_sequence.rb
+++ app/controllers/fibonacci_controller/fibonacci_sequence.rb
@@ -1,5 +1,13 @@
-class FibonacciSequence
-  def initialize(number)
-    @number = number
+class FibonacciController
+  class FibonacciSequence
+    after_initialize :calculate_sequence
+
+    def initialize(number)
+      @number = number
+    end
+
+    def calculate_sequence
+      puts @number
+    end
   end
 end
