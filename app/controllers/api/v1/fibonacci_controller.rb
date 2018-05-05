ce2c034e31e9a8b0f1208aee2cf8341297e88ffa
Gabriel Packer <ggkpacker@gmail.com>
2018-05-04 21:14:55 -0300

    add key to rendered json

--- app/controllers/api/v1/fibonacci_controller.rb
+++ app/controllers/api/v1/fibonacci_controller.rb
@@ -2,7 +2,7 @@ module Api::V1
  class FibonacciController < Api::V1::ApiController
     def fibonacci_sequence
       @sequence = FibonacciSequence.new(params[:quantidade])
-      render json: @sequence.calculate_sequence
+      render json: { sequence: @sequence.calculate_sequence }
     end
   end
 end
