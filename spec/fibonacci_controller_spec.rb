256682788cae581bb812915899e3cb29f8ca1ba6
Gabriel Packer <ggkpacker@gmail.com>
2018-05-04 21:15:31 -0300

    fix expectations with json parsing

--- spec/fibonacci_controller_spec.rb
+++ spec/fibonacci_controller_spec.rb
@@ -1,16 +1,33 @@
 require "rails_helper"
+require 'json'
 
 RSpec.describe "POST request to FibonacciController", type: :request do
 
   it "its valid when doing a request with 'quantidade' parameter" do
-    post "/fibonacci_sequence", params: { quantidade: 4 }
+    post "/api/v1/fibonacci_sequence", params: { quantidade: 4 }
 
     expect(response.status).to eq(200)
   end
 
-  # it "do a request without params" do
-  #   post "/fibonacci_sequence"
+  it "it should calculate fibonacci sequence with 2 sequence numbers" do
+    post "/api/v1/fibonacci_sequence", params: { quantidade: 2 }
 
-  #   response.code.should eq "200"
-  # end
+    data = JSON.parse(response.body)
+
+    expect(data['sequence']).to eq([0, 1])
+  end
+
+  it "it should calculate fibonacci sequence with 4 sequence numbers" do
+    post "/api/v1/fibonacci_sequence", params: { quantidade: 4 }
+
+    data = JSON.parse(response.body)
+    expect(data['sequence']).to eq([0, 1, 1, 2])
+  end
+
+  it "it should calculate fibonacci sequence with 6 sequence numbers" do
+    post "/api/v1/fibonacci_sequence", params: { quantidade: 6 }
+
+    data = JSON.parse(response.body)
+    expect(data['sequence']).to eq([0, 1, 1, 2, 3, 5])
+  end
 end
