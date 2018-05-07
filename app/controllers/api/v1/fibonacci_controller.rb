d7c6041f775cc06ded0255f3552b35559e683e38
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 12:41:21 -0300

    remove unused @ and calculate sequence in Fibonacci Sequence object

--- app/controllers/api/v1/fibonacci_controller.rb
+++ app/controllers/api/v1/fibonacci_controller.rb
@@ -1,8 +1,8 @@
 module Api::V1
  class FibonacciController < Api::V1::ApiController
     def fibonacci_sequence
-      @sequence = FibonacciSequence.new(params[:quantidade])
-      render json: { sequence: @sequence.calculate_sequence }
+      sequence = FibonacciSequence.new(params[:quantidade]).calculate_sequence
+      render json: { sequence: sequence }
     end
   end
 end
